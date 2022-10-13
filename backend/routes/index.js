528eced20c1b2c07bb371f85a52e045f2ad7d2b6
unknown <alexkana@csd.auth.gr>
2022-10-13 21:00:54 +0300

    Update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,30 +2,8 @@ import express from 'express';
 import axios from 'axios';
 
 const router = express.Router();
-const API_BASE_URL = process.env.API_BASE_URL;
-const API_LIST_PATH = process.env.API_LIST_URL;
 
 router.get('/markets', async (req,res) => {
-  /**   const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
-
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-
-    const results = {};
-
-    if (endIndex < finalRes.length)
-    results.next = {
-        page: page + 1,
-        limit: limit
-    }
-
-    if (startIndex > 0) {
-    results.previous = {
-        page: page - 1,
-        limit: limit
-     }
-    } */
 
     try{
     const apiRes = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`);
