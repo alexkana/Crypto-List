59749894ffa533203cbece1afa9ab7d0d43f6060
alexkana <56504716+alexkana@users.noreply.github.com>
2022-10-14 14:56:03 +0300

    Update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,11 @@
 import { StyledHeader, Nav, Logo, Image } from './styles/Header.styled'
 import { Container } from './styles/Container.styled'
-//import { Flex } from './styles/Flex.styled'
-//import { Button } from './styles/Button.styled'
 
 export default function Header() {
   return (
     <StyledHeader>
       <Container>
           <h1>Coin List</h1>
-     
       </Container>
     </StyledHeader>
   )
